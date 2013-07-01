25dffb54021d0c464390c4f1cb680dd30872ab35
Alvaro Lopez Ortega <alvaro@alobbs.com>
2013-07-01 22:01:21 +0200

    Adds a example test

--- test/test1.cpp
+++ test/test1.cpp
@@ -6,10 +6,25 @@
  */
 
 #include "gtest/gtest.h"
+#include <boost/locale.hpp>
+
+namespace loc = boost::locale;
+
+TEST(BoostUTF8, Basics) {
+	std::string UTF8String = u8"çlvaro L—pez Ortega";
+	EXPECT_EQ (UTF8String.size(),   19);
+	EXPECT_EQ (UTF8String.length(), 19);
+}
+
+TEST(BoostUTF8, LetterPosition) {
+	std::string UTF8String = u8"ca–—n";
+	EXPECT_EQ(UTF8String[3], '—');
+	EXPECT_EQ(UTF8String[4], 'n');
+}
 
 int
 main (int argc, char **argv)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
